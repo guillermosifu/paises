9a68a517385a0102cbc4880f9ae98d6d46ff72f0
guillermosifu <sifuentes1201@gmail.com>
2023-09-09 11:53:48 -0500

    curriencis

--- src/Detail/index,.jsx
+++ src/Detail/index,.jsx
@@ -1,54 +1,80 @@
-import{Container,Grid,Button} from "@mui/material"
+import { Container, Grid, Button } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useNavigate,useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getFlags } from "../service";
 
 const Detail = () => {
+  const history = useNavigate();
+  const { name } = useParams();
 
-  const history = useNavigate()  
-  const{name} = useParams()
+  const [country, setCountry] = useState({});
 
-  const[country,setCountry]=useState({})
-
-  const fecthCountry = async ()=>{
+  const fecthCountry = async () => {
     const response = await getFlags(
       `https://restcountries.com/v3.1/name/${name}`
-    )
-      setCountry(response[0])
-  }
-   
-  useEffect(()=>{
-    fecthCountry()
-  },[])
+    );
+    setCountry(response[0]);
+  };
 
-
+  useEffect(() => {
+    fecthCountry();
+  }, []);
 
   return (
-   <Container>
-    <Button variant="outlined" onClick={()=> history(-1)}>
+    <Container>
+      <Button variant="outlined" onClick={() => history(-1)}>
         Back
-    </Button>
-    {Object.keys(country).length >0 && (
-      <Grid container>
-        <Grid item md={6}>
-          <img src={country} alt="" />
-        </Grid>
-        <Grid item={6}>
-         <h3>{country.name.official}</h3>
-          <Grid container>
-            <Grid item={6}>
-          <p>Native Name :{country.name.official}</p>
-         </Grid>
-         <Grid item={6}>
-
-         </Grid>
+      </Button>
+      {Object.keys(country).length > 0 && (
+        <Grid
+          container
+          sx={{ height: "100vh" }}
+          alignItems="center"
+          spacing={3}
+        >
+          <Grid item md={6}>
+            <img src={country.flags.svg} width={400} alt="" />
           </Grid>
-         
+          <Grid item={6}>
+            <h3>{country.name.official}</h3>
+            <Grid container spacing={3}>
+              <Grid item={6}>
+                <p>
+                  <b>Native Name</b>:{country.name.common}
+                </p>
+                <p>
+                  <b>Population</b>:{country.population}
+                </p>
+                <p>
+                  <b>Region</b>: {country.region}
+                </p>
+                <p>
+                  <b>SubRegion</b>:{country.subregion}
+                </p>
+                <p>
+                  <b>Capital</b>:{country.capital}
+                </p>
+              </Grid>
+
+              
+              <Grid item={6}>
+                <p>
+                  <b>Top Level Domain</b>:{country.tld}
+                </p>
+                <p>
+                  <b>Currencies</b>
+                  {""}
+                  {Object.keys(country.currencies).map((currenci) => (
+                    <span>{currenci}</span>
+                  ))}
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
-    )}
-   </Container>
-  )
-}
+      )}
+    </Container>
+  );
+};
 
 export default Detail;
